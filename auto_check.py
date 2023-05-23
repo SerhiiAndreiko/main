daf9c8f9ab32a324c3aaca3d2ecd2cdc3dd125b7
SerhiiAndreiko <fantom784507@gmail.com>
2023-05-23 21:40:00 +0200

    Path

--- auto_check.py
+++ auto_check.py
@@ -1,12 +1,3 @@
-def format_ingredients(items):
-    recipe = ""
-    for i in range(len(items)):
-        if i == len(items) - 1:
-            recipe += " and " + items[i]
-        else:
-            recipe += items[i] + " "
-    return recipe
-
-items = ['2 eggs', '1 liter sugar', '1 tsp salt', 'vinegar']
-print(format_ingredients(items)), (items)
-
+from pathlib import Path
+p = Path()
+print (p)
